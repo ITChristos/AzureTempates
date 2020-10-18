bff95eda3c3b87d0916cf3f3fa3069424e3f9dfa
ITChristos <ITChristos@users.noreply.github.com>
2020-10-17 21:37:03 -0700

    Template Push

---
+++ AzureResourcePSTemplate.ps1
@@ -0,0 +1,57 @@
+
+
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+$location = Read-Host -Prompt "Enter the location (i.e. westus)"
+$storageAccountName = Read-Host -Prompt "Enter the storage account name"
+
+# Create the storage account.
+$storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroupName `
+    -Name $storageAccountName `
+    -Location $location `
+    -SkuName "Standard_LRS"
+
+# Retrieve the context.
+$ctx = $storageAccount.Context
+
+#Deploy template
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+$location = Read-Host -Prompt "Enter the location (i.e. centralus)"
+$templateUri = "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-storage-account-create/azuredeploy.json"
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -Location $location
+
+
+
+
+
+#Lock Resources (prevents other users in org from accidentally deleting or modifying critical resources)
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+$storageAccountName = Read-Host -Prompt "Enter the storage account name"
+
+New-AzResourceLock -LockName LockStorage -LockLevel CanNotDelete -ResourceGroupName $resourceGroupName -ResourceName $storageAccountName -ResourceType Microsoft.Storage/storageAccounts
+
+#Get all locks for a storage account
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+$storageAccountName = Read-Host -Prompt "Enter the storage account name"
+
+Get-AzResourceLock -ResourceGroupName $resourceGroupName -ResourceName $storageAccountName -ResourceType Microsoft.Storage/storageAccounts
+
+#Delete lock of a storage account
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+$storageAccountName = Read-Host -Prompt "Enter the storage account name"
+
+$lockId = (Get-AzResourceLock -ResourceGroupName $resourceGroupName -ResourceName $storageAccountName -ResourceType Microsoft.Storage/storageAccounts).LockId
+Remove-AzResourceLock -LockId $lockId
+
+#Move Resources
+$srcResourceGroupName = Read-Host -Prompt "Enter the source Resource Group name"
+$destResourceGroupName = Read-Host -Prompt "Enter the destination Resource Group name"
+$storageAccountName = Read-Host -Prompt "Enter the storage account name"
+
+$storageAccount = Get-AzResource -ResourceGroupName $srcResourceGroupName -ResourceName $storageAccountName
+Move-AzResource -DestinationResourceGroupName $destResourceGroupName -ResourceId $storageAccount.ResourceId
+
+#Delete Resources
+$resourceGroupName = Read-Host -Prompt "Enter the Resource Group name"
+$storageAccountName = Read-Host -Prompt "Enter the storage account name"
+
+Remove-AzStorageAccount -ResourceGroupName $resourceGroupName -AccountName $storageAccountName
